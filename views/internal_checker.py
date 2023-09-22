833dfd8c58230d00ef155a2b456307a1dd16c7dd
CelestialCrafter <celestialcraftermc@gmail.com>
2023-09-22 13:39:47 -0500

    fix redis hset??

--- views/internal_checker.py
+++ views/internal_checker.py
@@ -6,42 +6,47 @@ from flask import request
 from importlib import import_module
 from utils import get_algorithms
 
-redis_db = Redis(host='localhost', port=6379)
+redis = Redis(host='localhost', port=6379)
 
 def algorithm_output(algorithm, prices):
-  module = import_module(f'algorithms.{algorithm}')
-  signal = module.signal(module.algorithm(prices))
+	module = import_module(f'algorithms.{algorithm}')
+	signal = module.signal(module.algorithm(prices))
 
-  # @TODO strength
-  return algorithm, (signal, 0.5)
+	# @TODO strength
+	return algorithm, (signal, 0.5)
 
 def internal_checker():
-  if not ip_address(request.remote_addr).is_private:
-    return 'Forbidden', 403
-  
-  encoded = request.headers.get('Authorization')
-  if not encoded:
-    return 'Bad Request', 400
-  
+	if not ip_address(request.remote_addr).is_private:
+		return 'Forbidden', 403
+	
+	encoded = request.headers.get('Authorization')
+	if not encoded:
+		return 'Bad Request', 400
+	
  
-  try:
-    decoded = jwt.decode(encoded, os.environ['JWT_SECRET'], algorithms=['HS256'])
-  except Exception as e:
-    print(e)
-    return 'Unauthorized', 401
+	try:
+		decoded = jwt.decode(encoded, os.environ['JWT_SECRET'], algorithms=['HS256'])
+	except Exception as e:
+		print(e)
+		return 'Unauthorized', 401
 
-  if decoded['event'] != 'auth':
-    return 'Unauthorized', 401
+	if decoded['event'] != 'auth':
+		return 'Unauthorized', 401
 
-  config = json.load(open('config.json', 'r'))
-  prices = get_prices(config['pair'], interval=1440)
-  # Convert list of algorithms into {name: signal}
-  algorithms = get_algorithms()
+	config = json.load(open('config.json', 'r'))
+	prices = get_prices(config['pair'], interval=1440)
+	# Convert list of algorithms into {name: signal}
+	algorithms = get_algorithms()
 
-  results = json.dumps(dict(map(
-    lambda x: algorithm_output(*x),
-    zip(algorithms, [prices] * len(algorithms))
-  )))
-  redis_db.hset('data', 'current_data', results)
-  
-  return algorithms
+	base = dict(map(
+		lambda x: algorithm_output(*x),
+		zip(algorithms, [prices] * len(algorithms))
+	))
+	
+	signals = { k:v[0] for (k, v) in base.items() }
+	strengths = { k:v[1] for (k, v) in base.items() }
+
+	redis.hset('signals', mapping=signals)
+	redis.hset('strengths', mapping=strengths)
+	
+	return algorithms
