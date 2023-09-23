ad7b4f3f79ca14ad3814f48f1783726767fa2090
CelestialCrafter <celestialcraftermc@gmail.com>
2023-09-23 14:46:04 -0500

    change bot_profit to bot_net_worth move get_bot_profit to bot_net_worth require the right token for bot_net_worth a bit of styling

--- utils.py
+++ utils.py
@@ -1,14 +1,12 @@
 import os
 from pymongo.server_api import ServerApi
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = MongoClient(os.environ['DB_URI'], server_api=ServerApi('1'))
 algorithms = client['database']['algorithms']
-bots = client['database']['bots']
 
 def get_algorithms():
 	return [
@@ -16,11 +14,3 @@ def get_algorithms():
 		for algorithm
 		in algorithms.find({ 'owner': { '$not': { '$type': 'object' } } })
 	]
- 
-def get_bot_profit(bot_id):
-    bot_id = ObjectId(bot_id)
-    bot = bots.find_one({"_id": bot_id})
-    profits = bot['profits']
-
-    return profits
-
