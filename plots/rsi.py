823a5ba05a2f90a0ddb50c027c6eb5a17bdd758c
CelestialCrafter <celestialcraftermc@gmail.com>
2023-09-23 19:43:51 -0500

    added color api made all the plots use color api

--- plots/rsi.py
+++ plots/rsi.py
@@ -1,19 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from algorithms.rsi import algorithm as rsi
+import colors
 
 def plot(prices):
 	rsi_data = rsi(prices)
 	rsi_indicies = np.arange(0, rsi_data.shape[0])
 
-	plt.plot(rsi_indicies, rsi_data, color='plum')
+	plt.plot(rsi_indicies, rsi_data, color=colors.mainline())
 
 	# Thresholds
 	rsi_upper = np.full(rsi_data.shape, 70)
 	rsi_lower = np.full(rsi_data.shape, 30)
 
 	plt.fill_between(rsi_indicies, rsi_upper, rsi_lower, color='grey', alpha=0.3)
-	plt.plot(rsi_indicies, rsi_upper, linestyle='dashed', color='lightcoral')
-	plt.plot(rsi_indicies, rsi_lower, linestyle='dashed', color='darkturquoise')
+	plt.plot(rsi_indicies, rsi_upper, linestyle='dashed', color=colors.upper())
+	plt.plot(rsi_indicies, rsi_lower, linestyle='dashed', color=colors.lower())
 
 	return plt
