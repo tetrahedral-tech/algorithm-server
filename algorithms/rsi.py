dc0b73373c0007b647aa014e97c953834651cbc4
PrplFox <foxedex29@gmail.com>
2023-10-07 20:38:43 +0100

    fix

--- algorithms/rsi.py
+++ algorithms/rsi.py
@@ -3,7 +3,7 @@ from talib import RSI
 def algorithm(prices, window_size=14):
 	return RSI(prices, window_size)
 
-def signal(data, high=70, low=30):
+def signal(prices, data, high=70, low=30):
 	rsi = data
 	if rsi[-1] > high:
 		return 'sell'
