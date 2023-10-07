5c134baac0966cba078b9b834d1d452dec4e1ce8
PrplFox <foxedex29@gmail.com>
2023-10-07 07:33:57 +0100

    nothing

--- views/bot_net_worth.py
+++ views/bot_net_worth.py
@@ -11,16 +11,16 @@ def get_net_worth(bot_id):
 	return [worth['value'] for worth in bot_worth]
 
 def bot_net_worth(bot_id):
-	"""	jwt_encoded = request.headers.get('Authorization')
-		if not jwt_encoded:
-			return 'Bad Request', 400
+	jwt_encoded = request.headers.get('Authorization')
+	if not jwt_encoded:
+		return 'Bad Request', 400
 
-		try:
-			jwt_decoded = jwt.decode(jwt_encoded, os.environ['JWT_SECRET'], algorithms=['HS256'])
-			if jwt_decoded._id != bot_id:
-				raise 'Token Mismatch'
-		except Exception:
-			return 'Unauthorized', 401"""
+	try:
+		jwt_decoded = jwt.decode(jwt_encoded, os.environ['JWT_SECRET'], algorithms=['HS256'])
+		if jwt_decoded._id != bot_id:
+			raise 'Token Mismatch'
+	except Exception:
+		return 'Unauthorized', 401
 
 	plot(get_net_worth(bot_id))
 
