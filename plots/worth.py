262780340eb73bf4f451f1ecd4e3da7b6941753e
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-09 03:19:50 -0500

    rename indicies to arange

--- plots/worth.py
+++ plots/worth.py
@@ -6,7 +6,7 @@ import numpy as np
 def plot(timestamps, value):
 	value = np.array(value)
 	# times = [ dt.datetime.fromtimestamp(timestamp) for timestamp in timestamps ]
-	times = np.indicies(0, value.shape[0]) * 5
+	times = np.arange(0, value.shape[0]) * 5
 	plt.plot(times, value, color=colors.upper())
 
 	return plt
