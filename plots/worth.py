1fdc2aeb2b71482ea2f86422a8e459825b85386c
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-09 02:58:58 -0500

    add (disabled) timestamps to worth plot

--- plots/worth.py
+++ plots/worth.py
@@ -1,10 +1,12 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import plots.colors as colors
+import datetime as dt
+import numpy as np
 
-def plot(bot_profit):
-	bot_profit = np.array(bot_profit)
-	indicies = np.arange(0, bot_profit.shape[0]) * 5
-	plt.plot(indicies, bot_profit, color=colors.upper())
+def plot(timestamps, value):
+	value = np.array(value)
+	# times = [ dt.datetime.fromtimestamp(timestamp) for timestamp in timestamps ]
+	times = np.indicies(0, value.shape[0]) * 5
+	plt.plot(times, value, color=colors.upper())
 
 	return plt
