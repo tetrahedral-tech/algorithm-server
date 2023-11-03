70ff99d40976a2fd5a65bbdf8ac3e3a181ee860e
Fouad <108154224+fou3fou3@users.noreply.github.com>
2023-11-03 13:57:28 +0100

    algorithm update (#26)
    * rename profit/bot_net_worth to worth
    change how worth works
    
    * fix bollinger bands
    
    * add (disabled) timestamps to worth plot
    
    * Automated YAPF Formatting
    
    * rename indicies to arange
    
    * change graph colors and make them transparent
    
    * Automated YAPF Formatting
    
    * hopefully fix segfaults
    
    * update environment.yml
    
    * add dockerfile
    update packages
    
    * Create docker.yml
    
    * update server port to 80
    
    * remove builds from environment package
    allow for arm64 to install them
    
    * clean conda before installing in dockerfile
    
    * remove multiplatform (switched servers to x86)
    
    * add ta-lib submodule
    
    * remove unnecesary clean
    add gcc to environment to build ta-libn propperly
    
    * remove environment stuff from .dockerignore
    
    * modify dockerfile
    
    * change exported port from 5000 to 80
    
    * make rsi  conform to signal()
    
    * add price plot
    update rsi variable names
    
    * add price as algorithm
    
    * use redis uri instead of localhost for redis
    
    * simple mod
    
    * algo sync with mongo
    
    ---------
    
    Co-authored-by: CelestialCrafter <celestialcraftermc@gmail.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- views/plot.py
+++ views/plot.py
@@ -1,8 +1,13 @@
 import io
+import matplotlib
 import matplotlib.pyplot as plt
+import plots.colors as colors
+from utils import get_algorithms
 from price import get_prices
 from importlib import import_module
 from utils import get_algorithms
+
+matplotlib.use('Agg')
 
 def plot(algorithm):
 	prices = get_prices('ETH/USD', interval=1440)
@@ -11,9 +16,14 @@ def plot(algorithm):
 		return 'Invalid Algorithm', 404
 	import_module(f'plots.{algorithm}').plot(prices)
 
+	ax = plt.gca()
+	ax.tick_params(color=colors.outline(), labelcolor=colors.outline())
+	for spine in ax.spines.values():
+		spine.set_edgecolor(colors.outline())
+
 	# Save plot into buffer instead of the FS
 	svg_buffer = io.StringIO()
-	plt.savefig(svg_buffer, format='svg')
+	plt.savefig(svg_buffer, format='svg', transparent=True)
 	svg_plot = svg_buffer.getvalue()
 	plt.close()  # Solved plots overwriting each other
 	svg_buffer.close()
