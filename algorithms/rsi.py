99ed5d2d968e6200f8ae24b782b3ee0985b8b85d
PrplFox <foxedex29@gmail.com>
2023-11-04 08:22:32 +0100

    strength update

--- algorithms/rsi.py
+++ algorithms/rsi.py
@@ -6,7 +6,7 @@ def algorithm(prices, window_size=14):
 def signal(prices, data, high=70, low=30):
 	rsi = data
 	if rsi[-1] > high:
-		strength = rsi[-1] * (1 / low)
+		strength = ( rsi[-1] - high ) * ( 1 / low ) 
 		return 'sell', strength
 	elif rsi[-1] < low:
 		strength = 1 - rsi[-1] * (1 / low)
