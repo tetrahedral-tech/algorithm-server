082448f03978b7ba8e99b266fdeab39a25b53bec
CelestialCrafter <celestialcraftermc@gmail.com>
2023-11-20 02:42:57 -0600

    complete price caching add global interval var fix last commit not working

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
-from views import internal_checker, plot, worth
+from views import internal_checker, plot, worth, interval, update_interval
 
 load_dotenv()
 
@@ -9,8 +9,10 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 app.add_url_rule('/plot/<algorithm>', view_func=plot.plot)
-app.add_url_rule('/internal_checker', view_func=internal_checker.internal_checker)
 app.add_url_rule('/worth/<bot_id>', view_func=worth.worth)
+app.add_url_rule('/interval', view_func=interval.interval)
+app.add_url_rule('/internal_checker', view_func=internal_checker.internal_checker)
+app.add_url_rule('/update_interval', view_func=update_interval.update_interval, methods=['POST'])
 
 if __name__ == '__main__':
 	app.run()
