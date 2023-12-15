b97784cadd2a31615fee118bee55ff0bcb77e2d8
fox <fox@pop-os.localdomain>
2023-12-15 12:53:37 +0100

    macd implement

---
+++ algorithms/macd.py
@@ -0,0 +1,20 @@
+from talib import MACD
+import matplotlib.pyplot as plt
+import plots.colors as colors
+
+def algorithm(prices, fastperiod=12, slowperiod=26, signalperiod=9):
+	return MACD(prices, fastperiod=fastperiod, slowperiod=slowperiod, signalperiod=signalperiod) #macd, macdsignal, macdhist
+
+def signal(prices, data):
+	macd, signal, hist = data
+	if macd[-1] == signal[-1]:
+		return 'no_action', 0
+
+	elif macd[-2] == signal[-2]:
+		if macd[-1] > signal[-1]:
+			return 'buy', 0.5
+		elif signal[-1] > macd[-1]:
+			return 'sell', 1
+
+	else:
+		return 'no_action', 0
