5d6096c1838fcd45a1c450c79a9545e7bd5b839e
CelestialCrafter <celestialcraftermc@gmail.com>
2023-12-18 05:13:23 -0600

    color api changes

--- plots/colors.py
+++ plots/colors.py
@@ -1,14 +1,23 @@
-def mainline():
+def primary():
 	return 'plum'
 
-def secondaryline():
+def secondary():
 	return 'mediumpurple'
+
+def tertiary():
+	return 'mediumorchid'
 
 def upper():
 	return 'lightcoral'
 
+def uppersecondary():
+	return 'indianred'
+
 def lower():
 	return 'darkturquoise'
+
+def lowersecondary():
+	return 'skyblue'
 
 def inner():
 	return 'grey'
