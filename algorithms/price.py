a62831693759c1470091b60b9b13b75d744c2974
fou3fou3 <foxedex29@gmail.com>
2023-12-30 14:04:00 +0100

    rsi sell/buy points && plot price fix

--- algorithms/price.py
+++ algorithms/price.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import plots.colors as colors
 
 class Algorithm:
-
-	def plot(prices, timestamps):
+	def __init__(self) -> None:
+		pass
+	def plot(self, prices, timestamps):
 		plt.plot(timestamps, prices, color=colors.primary())
-		return plt
