82c5bcd2e76f527c58dd6ba58ed4b5f5754a44cf
fou3fou3 <foxedex29@gmail.com>
2023-12-30 14:04:00 +0100

    rsi sell/buy points && plot price fix

--- algorithms/rsi.py
+++ algorithms/rsi.py
@@ -2,6 +2,7 @@ from talib import RSI
 import numpy as np
 import matplotlib.pyplot as plt
 import plots.colors as colors
+from matplotlib.gridspec import GridSpec
 
 class Algorithm:
 
@@ -26,6 +27,9 @@ class Algorithm:
 		return 'no_action', 0
 
 	def plot(self, prices, timestamps, **kwargs):
+		gs = GridSpec(3, 1, figure=plt.gcf())
+
+		plt.subplot(gs[0, :])
 		rsi_line = self.algorithm(prices, **kwargs)
 
 		plt.plot(timestamps, rsi_line, color=colors.primary())
@@ -33,7 +37,14 @@ class Algorithm:
 		# Thresholds
 		upper = np.full(rsi_line.shape, self.high)
 		lower = np.full(rsi_line.shape, self.low)
+		upper_condition = rsi_line >= self.high
+		lower_condition = rsi_line <= self.low
 
 		plt.fill_between(timestamps, upper, lower, color='grey', alpha=0.3)
 		plt.plot(timestamps, upper, linestyle='dashed', color=colors.upper())
 		plt.plot(timestamps, lower, linestyle='dashed', color=colors.lower())
+
+		plt.subplot(gs[1, :])
+		plt.plot(timestamps, prices, color=colors.primary())
+		plt.scatter(timestamps[upper_condition], prices[upper_condition], color=colors.upper())
+		plt.scatter(timestamps[lower_condition], prices[lower_condition], color=colors.lower())
