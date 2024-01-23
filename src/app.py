52af980daa75fd3d8df4f4065ba7269ab55b366d
CelestialCrafter <celestialcraftermc@gmail.com>
2024-01-23 14:06:38 -0600

    swap to gunicorn

--- src/app.py
+++ src/app.py
@@ -1,5 +1,3 @@
-import waitress
-import os
 import schedule, time
 from threading import Thread
 from dotenv import load_dotenv
@@ -36,11 +34,7 @@ def start_price_cache():
 
 if __name__ == '__main__':
 	start_price_cache()
+	app.run()
 
-	if os.environ.get('WSGI') == '1':
-		listen_url = '0.0.0.0:80'
-		print('starting waitress...')
-		waitress.serve(app, listen=listen_url)
-		print(f'listening on {listen_url}')
-	else:
-		app.run()
+def get_app():
+	return app
