fe45820a85f6ee61c267dd11d45a9c0d6ca81609
PrplFox <foxedex29@gmail.com>
2024-01-29 20:09:03 +0100

    internal -> signals && signals mod

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
-from views import internal_checker, plot, worth, interval, update_interval, backtest
+from views import plot, signals, worth, interval, update_interval, backtest
 
 load_dotenv()
 
@@ -14,7 +14,7 @@ app.add_url_rule('/plot/<algorithm_name>', view_func=plot.plot)
 app.add_url_rule('/backtest/<algorithm_name>', view_func=backtest.backtest_view)
 app.add_url_rule('/worth/<bot_id>', view_func=worth.worth)
 app.add_url_rule('/interval', view_func=interval.interval)
-app.add_url_rule('/internal_checker', view_func=internal_checker.internal_checker)
+app.add_url_rule('/signals', view_func=signals.signals)
 app.add_url_rule('/update_interval', view_func=update_interval.update_interval, methods=['POST'])
 
 if __name__ == '__main__':
---
+++ src/views/signals.py
@@ -0,0 +1,35 @@
+import os
+from flask import request
+from redis import from_url
+from price import get_prices, get_default_interval, is_supported_interval, is_supported_interval
+from ipaddress import ip_address
+from utils import get_algorithms, algorithm_output
+
+redis = from_url(os.environ['REDIS_URI'])
+last_checked_point = 0
+
+def signals():
+	global last_checked_point
+	if not ip_address(request.remote_addr).is_private:
+		return 'Forbidden', 403
+
+	interval = int(request.args.get('interval') or get_default_interval())
+
+	if interval and is_supported_interval(interval):
+		prices, _, _ = get_prices(interval=interval)
+	elif not interval:
+		prices, _, _ = get_prices()
+	else:
+		return 'Unsupported Interval', 400
+
+
+	# Convert list of algorithms into {name: signal}
+	algorithms = get_algorithms()
+
+
+	base = dict(map(lambda x: algorithm_output(*x), zip(algorithms, [prices] * len(algorithms))))
+
+	signals = {k: v[0] for (k, v) in base.items()}
+	_ = {k: v[1] for (k, v) in base.items()} # Strengths (not used)
+
+	return signals
